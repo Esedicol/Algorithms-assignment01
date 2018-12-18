923c6a0d4cc1fb038464e62cfbf0478eda9783f3
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2018-12-18 15:33:38 +0000

    Update Term.java
    updated the constructor and added toString and equals methods

--- src/algo/Term.java
+++ src/algo/Term.java
@@ -1,20 +1,40 @@
 package algo;
 import java.util.ArrayList;
 
+//This is the template for the onjects which will contain a String Val and a Weight
 public class Term {
-	
 	public String value;
-    public double weight;
+	public double weight;
     
-
-	public Term()
+	//constructor
+	public Term(double weight, String theTerm)
 	{
-		
+		this.weight = weight;
+		this.theTerm = theTerm;
 	}
 
-	public ArrayList<Term> toLowerCase() {
-		// TODO Auto-generated method stub
-		return null;
+
+	@Override
+	public boolean equals(final Object obj)
+	{
+		if (obj instanceof Term)
+		{
+
+			final Term other = (Term) obj;
+			return Objects.equal(this.theTerm, other.theTerm);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	@Override
+	public String toString()
+	{
+		return toStringHelper(this).addValue(weight)
+				.addValue(theTerm).toString();
+
 	}
     
 
